79557bd577a59c4ad8e9de681d20ebfdebedf9a9
Rainer <braun707@genial.ms>
2020-09-02 00:54:30 +0200

    commit GileRain1

--- makeCommits.sh
+++ makeCommits.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 for ((i=1;i<=$1;i++))
 do
 date >> Gile$i.txt
-echo "commit GileRainer$i" >> Gile$i.txt
+echo "commit Gile$i" >> Gile$i.txt
+git add Gile$i.txt
 git commit -am "commit GileRain$i"
 done
-
 set abc=`date`
 date >> fileRai
